b1ddeaa7706380fcf1ac3762f53d4b24acc02378
dtaradai <madgohedkh@gmail.com>
2021-03-02 15:32:04 +0200

    add diagram MVC

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Logger\Logger;
 
 require_once 'functions.php';
@@ -8,15 +9,46 @@ $level = 'ERROR';
 $message = ' /\/\/5 ';
 
 $file = Logger::getInstance($message, $level);
-echo '<pre>';
-var_dump($file);
-echo '</pre>';
+
 
 use Figure\Rectangle;
 
-$rectangle = new Rectangle(5, 8 );
+//$rectangle = new Rectangle(5, 8 );
 // $triangle = new Triangle(5, 6, 7);
 // $circle = new Circle(5);
 
-echo $rectangle->square();
 
+//-----------Исключения-----------------
+
+function decimToBinary($decimal)
+{
+  if ($decimal == 0) {
+    return '0';
+  } elseif (is_int($decimal) && ($decimal > 0)) {
+    $binar = '';
+    $revers = '';
+
+    while ($decimal >= 1) {
+      $binar .= $decimal % 2;
+      $decimal /= 2;
+    }
+
+    for ($i = strlen($binar); $i >= 0; $i--) {
+      $revers .= substr($binar, $i, 1);
+    }
+    return $revers;
+  } else {
+    return 'Введите пожалуйста положительное число';
+  }
+}
+
+$decimal = 0;
+
+try {
+    if ($decimal == 0) {
+        throw new Exception('Вы ввели 0');
+    } 
+    echo decimToBinary($decimal);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
