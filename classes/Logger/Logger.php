22536f328981029b843933f1898e9d470187e389
dtaradai <madgohedkh@gmail.com>
2021-03-11 17:06:14 +0200

    fixed Logger

--- classes/Logger/Logger.php
+++ classes/Logger/Logger.php
@@ -4,28 +4,23 @@ namespace Logger;
 use \Exception;
 
 class Logger {
-  private $filePath;
-  private $date;
-  private $level;
-  private $message;
+  private static $filePath;
+  private static $date;
+  private static $level;
+  private static $message;
   private static $_logger = null;
 
   // Делаем невозможным создание объекта класса на прямую
-  // Здесь создаем объект текущего класса
-  private function __construct($message, $level = 'DEFAULT') {
-    // $this->filePath = 'logger.txt';
-    $this->messageVerification($message);
-    $this->levelVerification($level);
-    $this->writingToFile($this->date, $this->level, $this->message);
+  private function __construct() {
   }
 
   // Метод для создания объекта класса
-  public static function getInstance($message, $level) : Logger {
+  public static function getInstance($message, $level = 'DEFAUULT') : Logger {
+    self::messageVerification($message);
+    self::levelVerification($level);
+    self::writingToFile(self::$date, self::$level, self::$message);
 
-    // instanceof()
     if (self::$_logger === null) {
-      //Здесь срабатывает конструктор, при необходимости передаем в него необходимые параметры
-      // записываем наш объект в переменную
       self::$_logger = new Logger($message, $level);
     }
 
@@ -33,52 +28,38 @@ class Logger {
   }
 
 
-  private function levelVerification($level)
+  private static function levelVerification($level)
   {
 
     try {
       if ($level !== 'TRACE' && $level !== 'DEBUG' && $level !== 'INFO' && $level !== 'WARN' && $level !== 'ERROR' && $level !== 'FATAL' && $level !== 'DEFAULT') {
           throw new Exception('Error level');
       } 
-      $this->level = $level;
-      $this->date = date('Y-m-d H:m:s');
-  } catch (Exception $e) {
-      echo $e->getMessage();
+      self::$level = $level;
+      self::$date = date('Y-m-d H:m:s');
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
   }
 
-    // if ($level === 'TRACE' || $level === 'DEBUG' || $level === 'INFO' || $level === 'WARN' || $level === 'ERROR' || $level === 'FATAL' || $level === 'DEFAULT') {
-    //   $this->level = $level;
-    //   $this->date = date('Y-m-d H:m:s');
-    // } else {
-    //   exit('Error level');
-    // }
-  }
-
-  private function messageVerification($message)
+  private static function messageVerification($message)
   {
     try {
       if (empty($message)) {
         throw new Exception('Error message!');
       }
-      $this->message = htmlspecialchars(stripslashes(trim($message)));
+      self::$message = htmlspecialchars(stripslashes(trim($message)));
     } catch (Exception $e) {
       echo $e->getMessage();
     }
-
-    // if (!empty($message)) {
-    //   $this->message = htmlspecialchars(stripslashes(trim($message)));
-    // } else {
-    //   exit('Error message');
-    // }
   }
 
-  private function writingToFile($date, $level, $message)
+  private static function writingToFile($date, $level, $message)
   {
-    $fd = fopen($this->filePath == null ? $this->filePath = 'logger.txt' : $this->filePath, 'a');
-    // $fd = fopen($this->filePath, 'a') or die("не удалось открыть файл");
+    $fileDescriptor = fopen(self::$filePath == null ? self::$filePath = 'logger.log' : self::$filePath, 'a');
     $str = '(' . $date . ': ' . $message . ', ' . $level . ');'; // строка для записи
-    fwrite($fd, $str);            // запишем строку в начало
-    fclose($fd);                  // Закрываем файл
+    fwrite($fileDescriptor, $str);            // запишем строку в начало
+    fclose($fileDescriptor);                  // Закрываем файл
   }
 
   //oбъявляем для того, что бы невозможно было создать еще 1 объект этого же класса
